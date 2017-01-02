a31563dbb04a711dc06ce6abf8c3d3c5c3217986
Akhil <Akhi1@users.noreply.github.com>
2017-01-02 18:43:50 +0530

    Update DataTypesDemo.java

--- 30 days of Code Challenge/DataTypesDemo.java
+++ 30 days of Code Challenge/DataTypesDemo.java
@@ -4,7 +4,7 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-public class Solution {
+public class DataTypesDemo {
 
   public static void main(String[] args) {
       int i = 4;
