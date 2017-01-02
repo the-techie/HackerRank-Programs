cfb2a172c9c9260d3b6fef2fd3b183186c91bff1
Akhil <Akhi1@users.noreply.github.com>
2017-01-02 19:02:57 +0530

    Create DictionaryAndMaps.java

---
+++ 30 days of Code Challenge/DictionaryAndMaps.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+
+class DictionaryAndMaps {
+  public static void main(String []argh){
+    Scanner in = new Scanner(System.in);
+    int n = in.nextInt();
+    Map<String,Integer> phoneBook = new HashMap<String,Integer>();
+    for(int i = 0; i < n; i++){
+      String name = in.next();
+      int phone = in.nextInt();
+      // Write code here
+      //in.nextLine();
+      phoneBook.put(name, phone);
+    }
+    while(in.hasNext()){
+      String s = in.next();
+      // Write code here
+      if(phoneBook.containsKey(s)){
+          System.out.println(s+"="+phoneBook.get(s));
+      }
+      else{
+          System.out.println("Not found");
+      }
+    }
+    in.close();
+  }
+}
